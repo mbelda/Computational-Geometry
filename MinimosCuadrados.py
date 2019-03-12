94d3caab12173b69f4d8634b0e50a1f0cb83bb13
Maria José Belda <majo.belda15@gmail.com>
2019-03-12 12:49:58 +0100

    Generalizando dimensiones
    creaDatos generalizado para X, falta para T

--- MinimosCuadrados.py
+++ MinimosCuadrados.py
@@ -1,54 +1,114 @@
-## Author: MªJose Belda Beneyto
-## MINIMOS CUADRADOS 
-import numpy as np;
-import matplotlib.pyplot as plt;
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  2 10:36:01 2019
+
+@author: Maria Jose Belda Beneyto
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class MinimosCuadrados:
+    """
+    Atributos:
+        W: matriz del clasificador
+        
+    Metodos:
+        calculaW: calcula la matriz del clasificador dados unos puntos y sus
+            correspondientes clases
+        casificador: Usando W calcula las clases estimadas de los puntos que
+            se le pasan
+    """
+    
+    """
+    Inicialización de los atributos de la clase por defecto
+    """
+    def __init__(self):
+        self.W = []
+    
+    """
+    Calcula la matriz W asociada al clasificador mediante los argumentos de
+    entrada y la guarda en el atributo de la clase
+    
+    Entrada
+        X: Puntos
+        T: Clases asociadas a los puntos en X
+    """
+    def calculaW(self, X, T):
+        Xg = np.vstack((np.ones((1, X.shape[1])), X))
+        self.W = np.linalg.solve(Xg.dot(Xg.T),Xg.dot(T.T))
+    
+
+    """
+    Clasifica los puntos dados, es decir, calcula las etiquetas que les
+    corresponden usando la matriz W de la clase
+    
+    Entrada
+        puntos
+    """
+    def clasificador(self, puntos):
+        if self.W is []:
+            print("Entrena al clasificador")
+            return
+        filas, cols = puntos.shape
+        Puntosg = np.vstack((np.ones(cols), puntos))
+        T = (self.W.T).dot(Puntosg)
+        return np.argmax(T, axis=0)
+
+"""
+Crea datos aleatoriamente pero controlamos que los puntos de la misma clase
+tengan cierta relación para que el clasificador funcione razonablemente bien
 
 
-#Crea 100 datos de dim 2 (1 cosa de 2 filas 100 cols)
-#Y las medias
-#Necesitamos hacer la mierda esa con mu0 para poder sumar
-mu0 = np.array([10,0]);
-X0 = np.random.randn(2,100) + mu0[:,np.newaxis];
-mu1 = np.array([-10,10]);
-X1 = np.random.randn(2,100) + mu1[:,np.newaxis];
-mu2 = np.array([-10,-10]);
-X2 = np.random.randn(2,100) + mu2[:,np.newaxis];
-#Representamos los datos
-plt.plot(X0[0,:], X0[1,:], 'o'); #'o' para que pinte bolitas
-plt.plot(X1[0,:], X1[1,:], 'o');
-plt.plot(X2[0,:], X2[1,:], 'o');
-
-#Creamos las matrices ~X y T de la teoria",
-#hstack para apilar matrices horizontalmente (como añadir columnas)
-X = np.hstack((X0, X1, X2));
-Xg = np.vstack((np.ones((1, X.shape[1])), X));
-
-Unos = np.ones((1,100));
-Ceros = np.zeros((1,100));
-T1 = np.hstack((Unos,Ceros,Ceros));
-T2 = np.hstack((Ceros,Unos,Ceros));
-T3 = np.hstack((Ceros,Ceros,Unos));
-T = np.vstack((T1,T2,T3));
-
-#Calculamos W gorro
-Wg = np.linalg.inv(Xg.dot(Xg.T)).dot(Xg).dot(T.T);
-#No trasponemos (X*Xt) a la (-1) pq la mariz deberia ser sim asi que da igual
-#Aquí hemos invertido pero esto es ineficiente, deberiamos usar solve y resolver el sistema
-
-X = np.array([1,10,0]);
-Wg.T.dot(X);
-numpoints = 200;
-xs = np.linspace(-15,15,numpoints);
-ys = np.linspace(-15,15,numpoints);
-Xs, Ys = np.meshgrid(xs, ys);
-Xs.flatten();
-Ys.flatten();
-puntos = np.vstack((np.ones(numpoints**2), Xs.flatten(), Ys.flatten()));
-lev = Wg.T.dot(puntos);
-print(lev);
-#print(Sol.shape); (3,40000)
-
-#Ejercicio\n",
-#Este resultado que tenemos pintarlo, de forma que veamos cada punto en que clase esta y ver las fronteras de clasificacion\n",
-#Usar la funcion plt.contour
-#Transformamos quedandonos con 0, 1 en funcion de la coordenada maxima y usamos contour para pintarlo"
+Salida
+    X: matriz con los puntos creados
+    T: matriz con las etiquetas asociadas a los puntos en X
+"""    
+def creaDatos(K):
+    distClases = 10
+    ndatosxClase = 100
+    dispMedia = 0.8
+    
+    mus = [np.random.randn(2)*distClases]
+    for k in range(1, K):
+        auxMu = np.random.randn(2)*distClases
+        while min([np.linalg.norm(mu - auxMu) for mu in mus]) < 2*distClases:
+            auxMu = np.random.randn(2)*distClases
+        mus.append(auxMu)
+    
+    Ns = []
+    for k in range(0, K):
+        Ns.append(ndatosxClase)
+    N = sum(Ns)
+    
+    X = np.zeros((2, N))
+    cont = 0
+    for k in range(0, K):
+        X[:, cont:cont + Ns[k]] = np.random.randn(2, Ns[k])*dispMedia + mus[k][:, np.newaxis]
+        cont += Ns[k]
+    
+    
+    Unos = np.ones((1,100))
+    Ceros = np.zeros((1,100))
+    T1 = np.hstack((Unos,Ceros,Ceros))
+    T2 = np.hstack((Ceros,Unos,Ceros))
+    T3 = np.hstack((Ceros,Ceros,Unos))
+    T = np.vstack((T1,T2,T3))
+    return X, T
+    
+if __name__ == '__main__':
+    mcc = MinimosCuadrados()
+    K = 3
+    X, T = creaDatos(K)
+    mcc.calculaW(X, T)
+    
+    Xs = np.linspace(-15,15,200)
+    Ys = np.linspace(-15,15,200)
+    XX, YY = np.meshgrid(Xs, Ys)
+    x = XX.flatten()
+    y = YY.flatten()
+    puntos = np.vstack((x, y))
+      
+    ZZ = mcc.clasificador(puntos).reshape((200,200))
+    plt.plot(X[0, :], X[1, :], 'o')
+    plt.contour(XX, YY, ZZ)

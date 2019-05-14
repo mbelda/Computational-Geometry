59b81a32a4f1b29f93d89e81c294c20c7f3993b0
MiguelPascual13 <miguelpascual13@gmail.com>
2019-05-14 13:09:29 +0200

    PCA terminado, solo queda ponerlo bonito

--- PCA.py
+++ PCA.py
@@ -35,18 +35,22 @@ class PCA:
             conseguido = np.sum(D2[i:])/sumVarSing < umbral
             i = i + 1
         return i
-
-
-
-def mostrarDatos(U,Dprima,datos):
-    figura = plt.figure()
-    figura.subplots_adjust(hspace=0.4, wspace=0.4)
+    
+    def comprimirDatos(self,U,dprima,datos,media):
+        
+        Xcentrados = datos - media[:,np.newaxis]
+        
+        UUt = U[:,:dprima].dot(U[:,:dprima].T) 
+        
+        datosCompri = media[:,np.newaxis] + UUt.dot(Xcentrados)
+        return datosCompri
 
 
 if __name__ == '__main__':
     pca = PCA()
     mnist = loadmat(r'C:\Users\Miguel\mnist-original.mat')
     datos = mnist['data']
+    
     #Dimension 
     D = 784
     #Número de datos
@@ -62,6 +66,13 @@ if __name__ == '__main__':
     #Escogamos la dimensión D' mas pequeña que siga mateniendo una estructura razonable en función del umbral epsilon.
     dprima = pca.calculadprima(D2,D,umbral)
     #Comprimamos ahora todos los datos a la dimensión D' que es la óptima
-    
-    
-    
+    datosCompri= pca.comprimirDatos(U,dprima,datos,media)
+    #Mostremos parte de los datos Comprimido
+    fig, axs = plt.subplots(2, 4, figsize=(50, 50))
+    for k, ax in enumerate(axs[0, :]):
+        ax.imshow(datosCompri[:, k].reshape(28, 28), cmap='gray')
+        if k == 0: ax.set_ylabel('D\'=%d'.ljust(20)%dprima, fontsize=50, rotation=0)
+    #Mostramos la "misma" parte los datos originales
+    for k, ax in enumerate(axs[-1, :]):
+        ax.imshow(datos[:, k].reshape(28, 28), cmap='gray')
+        if k == 0: ax.set_ylabel('Original'.ljust(20), fontsize=50, rotation=0)
